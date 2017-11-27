475310a59a59dd48701b84eb17336618e9c05c26
Christian Enarsson <christianenarsson2@hotmail.com>
2017-11-27 14:21:58 +0100

    Add star rating system

--- code.js
+++ code.js
@@ -10,4 +10,20 @@ function renderMovie(movie){
 	document.querySelector("ul").innerHTML = actorList;
 };
 
+function changeStarRating(grade){
+  for (var i=1; i<=grade; i++){
+  	document.getElementById("s" + i).classList.add("filled");
+  	document.getElementById("s" + i).classList.remove("empty");
+  }
+  for (var j=grade+1; j<=5; j++){
+  	document.getElementById("s" + j).classList.add("empty");
+  	document.getElementById("s" + j).classList.remove("filled");
+  }
+}
+
+	for (let x=1; x<=5; x++) {
+		document.getElementById("s" + x).addEventListener("click", function(){ changeStarRating(x); });
+	}
+
+
 renderMovie(movieData);
