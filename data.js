5e776f4007a93f18cc9ec947ed8b5f5d858fc545
Christian Enarsson <christianenarsson2@hotmail.com>
2017-11-29 10:35:52 +0100

    Deleted .js-files

--- data.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  title : "Cyborg (1989)",
-  synopsis : "A martial artist hunts a killer in a plague-infested urban dump of the future. Also, there is a Cyborg...",
-  actors : ["Jean-Claude Van Damme", "Deborah Richter", "Vincent Klyn"],
-  poster : "https://images-na.ssl-images-amazon.com/images/M/MV5BMDk0MmYyMWMtOWVhNS00ZTM4LWI0NGEtMTMxYWFjYjVhNWVlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,642,1000_AL_.jpg"
- 
-};
