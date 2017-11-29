b5848a3486cf47f6d1d5916df6140d68fedee410
Christian Enarsson <christianenarsson2@hotmail.com>
2017-11-29 10:29:48 +0100

    Changed to Typescript!

---
+++ code.ts
@@ -0,0 +1,44 @@
+
+import {movieData} from "./data";
+import * as $ from "jquery";
+
+function renderMovie(movie){
+  $("h2").text(movie.title);
+  $("p").text(movie.synopsis);
+  $("img").attr("src", movie.poster);
+  
+    let actorList = "";
+  for(let i=0; i<movie.actors.length; i++){
+    actorList += "<li>" + movie.actors[i] + "</li>";
+}
+	$("ul").html(actorList);
+};
+
+function changeStarRating(grade){
+  for (var i=1; i<=grade; i++){
+  	$("#s" + i).addClass("filled");
+  	$("#s" + i).removeClass("empty");
+  }
+  for (var j=grade+1; j<=5; j++){
+  	$("#s" + j).addClass("empty");
+  	$("#s" + j).removeClass("filled");
+  }
+}
+
+renderMovie(movieData);
+
+
+for (let x=1; x<=5; x++) {
+  $("#s" + x).click(function(){changeStarRating(x);});
+  }
+
+
+
+
+
+
+
+
+
+
+
